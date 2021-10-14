d2d1160f257bd0f728970e64fb5e3d18da326edc
shuai <liershuai.cn@gmail.com>
2021-10-14 20:38:20 +0800

    项目初始化完成
    Signed-off-by: shuai <liershuai.cn@gmail.com>

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
-import {createApp} from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
 import store from './store'
+import router from './router'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css';
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(router).use(store).use(ElementPlus).mount('#app')
---
+++ src/views/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="nav">
+    <div class="Login">
+      <h1>Login</h1>
+    </div>
+    <router-link to="/home">Home</router-link>
+    |
+    <router-link to="/about">About</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login"
+}
+</script>
+
+<style scoped>
+
+</style>
