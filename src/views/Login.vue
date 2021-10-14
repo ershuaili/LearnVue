c91d36f8af82275b7096e54c653e6829164936cb
shuai <liershuai.cn@gmail.com>
2021-10-14 20:57:45 +0800

    项目初始化完成
    Signed-off-by: shuai <liershuai.cn@gmail.com>

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,6 +3,7 @@
     <div class="Login">
       <h1>Login</h1>
     </div>
+    <el-input v-model="input" placeholder="Please input"/>
     <router-link to="/home">Home</router-link>
     |
     <router-link to="/about">About</router-link>
